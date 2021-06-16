91adc82cd2f5fc444ac6e93643095644db16a690
Lxh6688 <1272662790@qq.com>
2021-06-16 23:49:14 +0800

    收集表单数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import {TagsSection} from './Money/TagsSection';
 import {CategorySection} from './Money/CategorySection';
@@ -7,17 +7,50 @@ import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 
 const MyLayout = styled(Layout)`
-  display:flex;
+  display: flex;
   flex-direction: column;
 `
 
+type Category = '-' | '+'
+
 function Money() {
+  const [selected, setSelected] = useState({
+    tags: [] as string[],
+    note: '',
+    category: '-' as Category,
+    amount: 0
+  })
   return (
     <MyLayout>
-      <TagsSection/>
-      <NoteSection/>
-      <CategorySection/>
-      <NumberPadSection/>
+      {selected.tags.join(',')}
+      <hr/>
+      {selected.note}
+      <hr/>
+      {selected.category}
+      <hr/>
+      {selected.amount}
+      <TagsSection value={selected.tags}
+                   onChange={(tags) => setSelected({
+                     ...selected,
+                     tags: tags
+                   })}/>
+      <NoteSection value={selected.note}
+                   onChange={(note) => setSelected({
+                     ...selected,
+                     note: note
+                   })}/>
+      <CategorySection value={selected.category}
+                       onChange={(category) => setSelected({
+                         ...selected,
+                         category: category
+                       })}/>
+      <NumberPadSection value={selected.amount}
+                        onChange={(amount) => setSelected({
+                          ...selected,
+                          amount: amount
+                        })}
+                        onOK={() =>{}}
+      />
     </MyLayout>
   )
 }
