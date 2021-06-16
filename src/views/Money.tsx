1d017dc12a169ad030907e920e12cdfb65ed16c1
Lxh6688 <1272662790@qq.com>
2021-06-16 23:57:49 +0800

    重构表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -20,35 +20,20 @@ function Money() {
     category: '-' as Category,
     amount: 0
   })
+  type Selected = typeof selected;
+  const onChange = (obj: Partial<Selected>)=>{
+    setSelected({ ...selected, ...obj })
+  }
   return (
     <MyLayout>
-      {selected.tags.join(',')}
-      <hr/>
-      {selected.note}
-      <hr/>
-      {selected.category}
-      <hr/>
-      {selected.amount}
       <TagsSection value={selected.tags}
-                   onChange={(tags) => setSelected({
-                     ...selected,
-                     tags: tags
-                   })}/>
+                   onChange={tags => onChange({tags})}/>
       <NoteSection value={selected.note}
-                   onChange={(note) => setSelected({
-                     ...selected,
-                     note: note
-                   })}/>
+                   onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category) => setSelected({
-                         ...selected,
-                         category: category
-                       })}/>
+                       onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={(amount) => setSelected({
-                          ...selected,
-                          amount: amount
-                        })}
+                        onChange={amount => onChange({amount})}
                         onOK={() =>{}}
       />
     </MyLayout>
