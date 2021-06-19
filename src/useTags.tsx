22f62f73b498a8bb21d30a5bc8a99629a91f748e
Lxh6688 <1272662790@qq.com>
2021-06-19 15:53:54 +0800

    封装 findTag 到 useTags 里

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,14 +1,16 @@
 import {useState} from "react";
 import {createdId} from "lib/createdId";
 
+const defaultTags = [
+  {id: createdId(), name: '衣'},
+  {id: createdId(), name: '食'},
+  {id: createdId(), name: '住'},
+  {id: createdId(), name: '行'},
+]
 const useTags = () => { // 封装一个自定义 Hook
-  const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    {id: createdId(), name: '衣'},
-    {id: createdId(), name: '食'},
-    {id: createdId(), name: '住'},
-    {id: createdId(), name: '行'},
-  ]);
-  return {tags, setTags}
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0]
+  return {tags, setTags, findTag}
 }
 
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import {useTags} from "../useTags";
+import {useParams} from "react-router-dom"
 
-const Tag:React.FC = () => {
+type Params = {
+  id: string
+}
+const Tag:React.FC = (props) => {
+  const {findTag} = useTags()
+  let {id} = useParams<Params>()
+
+  const tag = findTag(parseInt(id))
   return (
     <div>hi</div>
   )
